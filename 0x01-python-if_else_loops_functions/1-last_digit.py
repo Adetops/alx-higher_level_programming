8c35608ff4117ec427073b852602af543383710d
Adetops <heritagediwamu@gmail.com>
2023-06-06 16:14:13 -0700

    Use of abs() function

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+l_digit = abs(number) % 10
+if number < 0:
+    l_digit = -l_digit
+if l_digit > 5:
+    l_str = "greater than 5"
+elif l_digit == 0:
+    l_str = "0"
+else:
+    l_str = "less than 6 and not 0"
+print("Last digit of {} is {} and is {}".format(number, l_digit, l_str))
