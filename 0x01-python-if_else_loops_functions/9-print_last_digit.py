129d9c602511adb5a5fa95f310ae5cd7abc0a45c
Adetops <heritagediwamu@gmail.com>
2023-06-06 19:19:35 -0700

    Removed the main test

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,11 +3,3 @@ def print_last_digit(number):
     digit = abs(number) % 10
     print(digit, end='')
     return digit
-
-
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
