1ca4621be67a9b2f24ed8852e225f8f4e3b681ef
Adetops <heritagediwamu@gmail.com>
2023-06-10 22:56:28 -0700

    Matrix print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(matrix[row])):
             print("{:d}".format(matrix[row][i]), end=" ")
             if (matrix[row][i] % 3 == 0):
-                print(matrix[row][i], end="")
+                print("{:d}".format(matrix[row][i]), end="")
         print()
