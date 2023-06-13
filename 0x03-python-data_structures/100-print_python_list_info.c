fc101313ae24a4f7a8390414952b46b807f5ccee
Adetops <heritagediwamu@gmail.com>
2023-06-12 20:55:51 -0700

    Betty satisfied

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,6 +2,11 @@
 #include <object.h>
 #include <listobject.h>
 
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: pointer to the list object
+ * Return: nothing
+ */
 void print_python_list_info(PyObject *p)
 {
 	long int size = PyList_Size(p);
