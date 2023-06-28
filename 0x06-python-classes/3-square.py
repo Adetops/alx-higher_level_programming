fdf46acd87037416a8cd84478930901cfb94fc7e
Adetops <heritagediwamu@gmail.com>
2023-06-28 16:19:32 -0700

    Return area from area instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Define a square:"""
+
+
+class Square:
+    """implementing the class square """
+
+    def __init__(self, size=0):
+        """ Initializing the square now
+
+        Args:
+            size: the size of the square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Public instance method """
+        return self.__size ** 2
