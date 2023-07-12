bf5cde282668dc792ec0a4a4b803943a5710330f
Adetops <heritagediwamu@gmail.com>
2023-07-12 11:23:15 -0700

    Pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,10 +4,13 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     '''A class that inherits from BaseGeometry'''
 
     def __init__(self, width, height):
+        ''' Instatiation of subclass Rectangle '''
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
