0c6e727dd37e744183d662a3f29d45c80970c538
Adetops <heritagediwamu@gmail.com>
2023-07-12 05:40:16 +0100

    Pycodestyle

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,5 +10,6 @@ def save_to_json_file(my_obj, filename):
         my_obj (obj): any data structure
         filename (str): name of the file to write to
     '''
+
     with open(filename, mode='w', encoding='UTF8') as f:
         json.dump(my_obj, f)
