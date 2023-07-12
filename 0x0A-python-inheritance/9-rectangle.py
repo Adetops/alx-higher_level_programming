6e160873a37771cfd4ac0e418b1cd7bdcbd427c2
Adetops <heritagediwamu@gmail.com>
2023-07-12 11:32:31 -0700

    Not done

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Modules that defines the class Rectangle '''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''A class that inherits from BaseGeometry'''
+
+    def __init__(self, width, height):
+        ''' Instatiation of subclass Rectangle '''
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__heigth = height
