7f97191653798c7373d10116cbaec23453b0f809
Adetops <heritagediwamu@gmail.com>
2023-07-15 22:53:45 +0100

    Main files created

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+Class Base:
+    __nb_objects = 0
+    def  __init__(self, id=None):
+        if (id != None):
+            self.id = id
+        if (id == None):
+            __nb_objects += 1
+            self.id = __nb_objects
