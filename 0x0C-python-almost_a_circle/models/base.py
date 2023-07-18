a588902bf9eabe7add73b7584987bf1e898c4cdb
Adetops <heritagediwamu@gmail.com>
2023-07-17 19:53:24 -0700

    Not done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,6 +27,21 @@ class Base:
             The JSON string representation
         '''
 
-        if (list_dictionaries is None):
-            return ("[]")
-        return (json.dumps(list_dictionaries))
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' writes the JSON string representation of list_objs to a file '''
+
+        filename = cls.__name__ + ".json"
+        list_obj = []
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write('[]')
+            if list_objs is not None:
+                for i in range(len(list_objs)):
+                    json_not = Base.to_json_string(list_objs[i])
+                    list_obj.append(json_not)
+            f.write(list_obj)
