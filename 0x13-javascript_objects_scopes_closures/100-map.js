e6289166df4cd0cba6bf6f284492c2cfaca11643
Adetops <heritagediwamu@gmail.com>
2023-09-14 04:55:02 +0000

    Advanced tasks

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const list = require('./100-data').list;
+const multiIdx = (value, index) => value * index;
+const newlist = list.map(multiIdx);
+console.log(list);
+console.log(newlist);
